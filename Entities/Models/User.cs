e5442f0e0c964462c8ca593cc9b65d0fab3ac454
Le Duy Hai <haileduy72@gmail.com>
2025-09-18 20:52:34 +0700

    done register and login api

---
+++ Entities/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Entities.Models;
+
+public partial class User
+{
+    public int Id { get; set; }
+
+    public string Username { get; set; } = null!;
+
+    public string Password { get; set; } = null!;
+
+    public string? Name { get; set; }
+
+    public string? Gender { get; set; }
+
+    public int Role { get; set; }
+}
---
+++ Repositories/Authentication/IAuthentiicationRepository.cs
@@ -0,0 +1,17 @@
+﻿using DataTranferObjects.Login;
+using Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories.Authentication
+{
+    public interface IAuthentiicationRepository
+    {
+        public User? Login(LoginDTO loginDto);
+        public User Register(RegisterDTO registerDto);
+    }
+}
